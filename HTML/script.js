bfcde641604d83f4b2fe09c4f7f7d3d042ee46ff
kiarash <kiarashkianid@yahoo.com>
2025-03-02 22:05:31 -0500

    project structure

--- HTML/script.js
+++
@@ -1,8 +0,0 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-xmark');
-    navbar.classList.toggle('active')
-}
-
